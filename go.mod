816ce5848fe91893fa0c12f2dcd3a159737a3ad4
blobo <ixdoggo@hotmail.com>
2024-06-18 18:09:37 -0500

    deleted:    arf.exe 	modified:   go.mod 	modified:   go.sum 	modified:   main.go 	new file:   packages.toml

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module arf
 
 go 1.22.4
 
-require github.com/fred1268/go-clap v1.2.1 // indirect
+require (
+	github.com/BurntSushi/toml v1.4.0
+	github.com/fred1268/go-clap v1.2.1 // indirect
+)
--- main.go
+++ main.go
@@ -2,21 +2,34 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/BurntSushi/toml"
 )
 
-func main() {
+type Config struct {
+	name    string
+	version int
+}
 
+func main() {
+	var config Config
 	var oper string
 	var pkg string
+	_, err := toml.DecodeFile("packages.toml", &config)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("-----|arfpkg V1.1|-----")
+	fmt.Println(config.name)
 	fmt.Println("Enter operation ")
 	fmt.Scanln(&oper)
-
 	if oper == "install" {
 		fmt.Println("Enter package name: ")
 		fmt.Scanln(&pkg)
 
 		fmt.Printf("Package not found.")
 	}
-	fmt.Printf("\nexiting...")
+	fmt.Printf("\nexiting... ")
+	os.Exit(0)
 }
